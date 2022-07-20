176bee5dea02a9918541a5485bf11f8c4b402888
Peter Sekesan <72456128+Pete-Sekesan@users.noreply.github.com>
2022-07-20 08:48:47 -0400

    Initial Commit

---
+++ A Simple Game of War/A_Simple_Game_of_WarApp.swift
@@ -0,0 +1,17 @@
+//
+//  A_Simple_Game_of_WarApp.swift
+//  A Simple Game of War
+//
+//  Created by Peter Sekesan on 7/20/22.
+//
+
+import SwiftUI
+
+@main
+struct A_Simple_Game_of_WarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A Simple Game of War/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  A Simple Game of War
+//
+//  Created by Peter Sekesan on 7/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
